33f96a85708ecc8b48fd8dafff9dff99cc3e2eb8
Bruno Hammerschmidt <re4deto@hotmail.com>
2020-02-10 23:16:35 -0300

    nome do modelo adicionado

--- src/Models/Caminhoes/Caminhao.cs
+++ src/Models/Caminhoes/Caminhao.cs
@@ -5,6 +5,7 @@ namespace Models.Caminhoes
     public class Caminhao : BaseModel
     {
         public ModeloCaminhao Modelo { get; set; }
+        public string? NomeModelo => Enum.GetName(typeof(ModeloCaminhao), Modelo);
         public int AnoFabricacao { get; set; }
         public int AnoModelo { get; set; }
     }
